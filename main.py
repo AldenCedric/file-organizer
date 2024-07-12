6d879a802c179e16ac176db4433e14d8e6083dcb
alden <aldencedric314@gmail.com>
2024-07-12 13:11:09 +0800

    check files

--- main.py
+++ main.py
@@ -9,3 +9,18 @@ TARGET_DIR = ""
 def listFiles(target_dir):
     files = listdir(target_dir)
     return files
+
+# check files (folders or files)
+def checkFile(target_dir):
+    folders = []
+    files = []
+    files_list = listFiles(target_dir)
+
+    for i in files_list:
+        try:
+            listFiles(path.join(target_dir, i))
+            folders.append(i)
+        except:
+            files.append(i)
+
+    return{"folders": folders, "files": files}
